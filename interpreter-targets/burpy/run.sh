e314231708540354eb8c96578c7f3092acff1a8e
AnHyoungBin <nks00404@gmail.com>
2023-10-12 11:59:51 +0900

    Delete interpreter-targets directory

--- interpreter-targets/burpy/run.sh
+++
@@ -1,7 +0,0 @@
-service mysql start;
-mysql -u root < /example.sql
-service mysql stop;
-
-tail -f  /var/log/apache2/error.log &
-
-supervisord
--- interpreter-targets/oscommerce/oscommerce2/includes/modules/boxes/templates/categories.php
+++
@@ -1,7 +0,0 @@
-<?php
-use OSC\OM\OSCOM;
-?>
-<div class="panel panel-default">
-  <div class="panel-heading"><?php echo OSCOM::getDef('module_boxes_categories_box_title'); ?></div>
-  <?php echo $category_tree; ?>
-</div>
